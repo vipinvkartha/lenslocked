0fd144557337fc107099f25fe00546f671aef267
vipinvkartha <vipin0292@gmail.com>
2024-12-24 16:48:44 +0100

    sessions

--- main.go
+++ main.go
@@ -36,10 +36,14 @@ func main() {
 	userService := models.UserService{
 		DB: db,
 	}
+	sessionService := models.SessionService{
+		DB: db,
+	}
 
 	// Setup our controllers
 	usersC := controllers.Users{
-		UserService: &userService,
+		UserService:    &userService,
+		SessionService: &sessionService,
 	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(
---
+++ models/session.go
@@ -0,0 +1,83 @@
+package models
+
+import (
+	"crypto/sha256"
+	"database/sql"
+	"encoding/base64"
+	"fmt"
+
+	"github.com/vipinvkartha/lenslocked/rand"
+)
+
+const (
+	// The minimum number of bytes to be used for each session token.
+	MinBytesPerToken = 32
+)
+
+type Session struct {
+	ID     int
+	UserID int
+	// Token is only set when creating a new session. When looking up a session
+	// this will be left empty, as we only store the hash of a session token
+	// in our database and we cannot reverse it into a raw token.
+	Token     string
+	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+	// BytesPerToken is used to determine how many bytes to use when generating
+	// each session token. If this value is not set or is less than the
+	// MinBytesPerToken const it will be ignored and MinBytesPerToken will be
+	// used.
+	BytesPerToken int
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	bytesPerToken := ss.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+	token, err := rand.String(bytesPerToken)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	// TODO: Hash the session token
+	session := Session{
+		UserID:    userID,
+		Token:     token,
+		TokenHash: ss.hash(token),
+	}
+	row := ss.DB.QueryRow(`
+		UPDATE sessions
+		SET token_hash = $2
+		WHERE user_id = $1
+    RETURNING id;`, session.UserID, session.TokenHash)
+	err = row.Scan(&session.ID)
+	if err == sql.ErrNoRows {
+		// If no session exists, we will get ErrNoRows. That means we need to
+		// create a session object for that user.
+		row = ss.DB.QueryRow(`
+			INSERT INTO sessions (user_id, token_hash)
+			VALUES ($1, $2)
+			RETURNING id;`, session.UserID, session.TokenHash)
+		// The error will be overwritten with either a new error, or nil
+		err = row.Scan(&session.ID)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	// TODO: Store the session in our DB
+	return &session, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	// TODO: Implement SessionService.User
+	return nil, nil
+}
+
+func (ss *SessionService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	// base64 encode the data into a string
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
+}
